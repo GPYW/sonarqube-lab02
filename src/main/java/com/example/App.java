9d37f22e26d3b5a92df8774268cc04c7d6af3131
GPYW <pasindiyathra@gmail.com>
2026-02-01 23:29:28 +0530

    correction2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,7 +8,10 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Calculator calc = new Calculator();
-        LOGGER.info(String.valueOf(calc.calculate(10, 5, "add-again")));
+        if (LOGGER.isLoggable(Level.INFO)) {
+            int result = calc.calculate(10, 5, Operation.ADD);
+            LOGGER.log(Level.INFO, "Calculation result: {0}", result);
+        }
 
         UserService service = new UserService();
         service.findUser("admin");
